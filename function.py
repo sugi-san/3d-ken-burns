fb7a1ac43878685351276e48da4a73fe2a5dc02a
sugi-san <90301727+sugi-san@users.noreply.github.com>
2022-05-13 12:50:15 +0900

    Add files via upload

---
+++ function.py
@@ -0,0 +1,53 @@
+
+# --- display_pic ---
+import matplotlib.pyplot as plt
+from PIL import Image
+import numpy as np
+import os
+import glob
+
+def display_pic(folder):
+    fig = plt.figure(figsize=(30, 60))
+    files = sorted(glob.glob(folder+'/*.jpg'))
+    for i, file in enumerate(files):
+        img = Image.open(file)    
+        images = np.asarray(img)
+        ax = fig.add_subplot(10, 5, i+1, xticks=[], yticks=[])
+        image_plt = np.array(images)
+        ax.imshow(image_plt)
+        name = os.path.basename(file)
+        ax.set_xlabel(name, fontsize=30)               
+    plt.show()
+    plt.close()
+
+
+# --- reset_folder ---
+import shutil
+
+def reset_folder(path):
+    if os.path.isdir(path):
+      shutil.rmtree(path)
+    os.makedirs(path,exist_ok=True)
+    
+    
+# --- size adjust --- 
+import cv2
+
+def size_opt(file):
+    max_size = 1024
+    npyImage = cv2.imread(filename = file, flags = cv2.IMREAD_COLOR)
+    intWidth = npyImage.shape[1]
+    intHeight = npyImage.shape[0]
+
+    fltRatio = float(intWidth) / float(intHeight)
+    intWidth = min(int(max_size * fltRatio), max_size)
+    intHeight = min(int(max_size / fltRatio), max_size)
+
+    # make even
+    if not intWidth % 2 == 0:
+      intWidth +=1
+    if not intHeight % 2 ==0:
+      intHeight +=1
+
+    npyImage = cv2.resize(src=npyImage, dsize=(intWidth, intHeight), fx=0.0, fy=0.0, interpolation=cv2.INTER_AREA)
+    cv2.imwrite(file, npyImage)
