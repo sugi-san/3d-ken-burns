d208cbf3da44c3ef52aed381fbc4d600ff466391
sugi-san <90301727+sugi-san@users.noreply.github.com>
2022-05-13 13:22:49 +0900

    Update function.py

--- function.py
+++ function.py
@@ -1,3 +1,17 @@
+# --- display_mp4 ---
+from IPython.display import display, HTML
+from IPython.display import HTML
+
+def display_mp4(path):
+    from base64 import b64encode
+    mp4 = open(path,'rb').read()
+    data_url = "data:video/mp4;base64," + b64encode(mp4).decode()
+    display(HTML("""
+    <video width=700 controls>
+        <source src="%s" type="video/mp4">
+    </video>
+    """ % data_url))
+
 
 # --- display_pic ---
 import matplotlib.pyplot as plt
